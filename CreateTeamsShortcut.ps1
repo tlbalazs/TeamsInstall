460a35868cac75540f01dd8ba82d2ff0617ab0f6
tlbalazs <tothlaszlo.b@gmail.com>
2023-11-02 13:24:40 +0100

    Add files via upload

--- CreateTeamsShortcut.ps1
+++ CreateTeamsShortcut.ps1
@@ -18,11 +18,11 @@ try
     if($shortcutExists -eq $false -and $AppXStatus -ne $null)
     {
         Copy-Item $sourcePath $destinationPath -Force
+        Remove-Item "C:\Users\$username\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Microsoft Teams.lnk"
         Write-Eventlog -LogName Application -Source $logSource -EntryType Information -EventId 100 -Message "Teams shortcut has been copied to the Desktop."
     }
     else
     {
-        ##The shortcut already exists
         Write-Eventlog -LogName Application -Source $logSource -EntryType Information -EventId 100 -Message "The shortcut already exists."
     }
 }
